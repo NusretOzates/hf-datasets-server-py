9f70a3aff1bea8c9c72777ccf6efd1e77baa1d15
nusretozates <m.nusret.ozates@gmail.com>
2022-09-24 20:44:42 +0300

    Library name changed due to a mistake when adding the library into pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import find_packages, setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setup(
-    name='datasets_server_py',
+    name='datasets_server_python',
     packages=find_packages(include=['datasets_server_py']),
     version='0.1.0',
     description='Python SDK to access the Datasets Server',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['datasets', 'huggingface'],
     author='Nusret Ozates',
     license='MIT',
     install_requires=["requests", "pydantic"],
