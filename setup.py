5968f6a4bc3770aeb3a4841883939fb81a4c5f6f
nusretozates <m.nusret.ozates@gmail.com>
2022-10-09 16:39:56 +0300

    Add new make rule to push reformatted code, code reformatted

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
 
 setup(
-    name='datasets_server_python',
-    packages=find_packages(include=['datasets_server_py']),
-    version='0.2.0',
-    description='Python SDK to access the Datasets Server',
+    name="datasets_server_python",
+    packages=find_packages(include=["datasets_server_py"]),
+    version="0.2.0",
+    description="Python SDK to access the Datasets Server",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords=['datasets', 'huggingface'],
-    author='Nusret Ozates',
-    license='MIT',
+    long_description_content_type="text/markdown",
+    keywords=["datasets", "huggingface"],
+    author="Nusret Ozates",
+    license="MIT",
     install_requires=["requests", "pydantic"],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==7.1.3'],
-    test_suite='tests',
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest==7.1.3"],
+    test_suite="tests",
 )
